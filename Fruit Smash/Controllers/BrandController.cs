42c9692bef4f9b5788b79da7a75b5217c77add09
Sarah <smiths37@gmail.com>
2022-02-18 14:33:49 -0800

    Add project files.

---
+++ Fruit Smash/Controllers/BrandController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Fruit_Smash.Models;
+
+namespace Fruit_Smash.Controllers
+{
+    public class BrandController : Controller
+    { 
+        public IActionResult Index()
+        {
+            //Replicating an instance of the brand class, to mimic getting data from a CMS
+            //It looks like you are using Episerver - I have experience using Ektron, which Episerver bought/acquired, before they became Optimizely
+            //I'm going off my knowledge of Ektron - you could create "smartforms" and assign ids to each field
+            //Then you'd get that data using Ektron's user controls, deserialize the JSON, and finally assign those fields' values to an instance of a class
+            //This is what I'm trying to mimic here - the text values would be whatever value you could get from the smartform/cms object data
+            Brand fruitSmash = new Brand();
+            fruitSmash.BrandName = "Fruit Smash";
+            fruitSmash.CanImageUrl = "/images/cans.png";
+            fruitSmash.PackImageUrl = "/images/pack.jpg";
+            fruitSmash.HeroImageUrl = "/images/hero.jpg";
+            fruitSmash.HeroImageMobileUrl = "/images/hero-mobile.webp";
+            fruitSmash.HeroImageTabletUrl = "/images/hero-tablet.png";
+            fruitSmash.TagLineHeading = "Say Bye";
+            fruitSmash.TagLineSubHeading = "To Basic Selterzing";
+            fruitSmash.CtaButton1Text = "Find near you";
+            fruitSmash.Cta1ButtonUrl = "https://www.fruitsmashseltzer.com/#find";
+            fruitSmash.CtaButton2Text = "Learn More";
+            fruitSmash.Cta2ButtonUrl = "https://www.fruitsmashseltzer.com/";
+            fruitSmash.Cta2Heading = "Super Hard Variety Pack";
+            fruitSmash.Cta2Text = ViewBag.HtmlStr = "Introducing <strong>The New</strong> Fruit Smash Super Hard Seltzer. A boozy seltzer made with real juice for a bolder taste that packs a punch.";
+
+            return View(fruitSmash);
+        }
+    }
+}
---
+++ Fruit Smash/Models/Brand.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fruit_Smash.Models
+{
+    public class Brand
+    {
+        [Required]
+        public string BrandName { get; set; }
+        [Required]
+        public string TagLineHeading { get; set; }
+        [Required]
+        public string TagLineSubHeading { get; set; }
+        [Required]
+        public string CtaButton1Text { get; set; }
+        [Required]
+        public string CtaButton2Text { get; set; }
+        [Required]
+        public string Cta2Heading { get; set; }
+        [Required]
+        public string Cta2Text { get; set; }
+        [Required]
+        public string Cta1ButtonUrl { get; set; }
+        [Required]
+        public string Cta2ButtonUrl { get; set; }
+        [Required]
+        public string HeroImageUrl { get; set; }
+        [Required]
+        public string HeroImageMobileUrl { get; set; }
+        [Required]
+        public string HeroImageTabletUrl { get; set; }
+        [Required]
+        public string CanImageUrl { get; set; }
+        [Required]
+        public string PackImageUrl { get; set; }
+    }
+}
