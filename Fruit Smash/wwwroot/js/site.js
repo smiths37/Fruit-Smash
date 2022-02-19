af5a579430a0684ddab5e2ae71eda20dea9f5511
Sarah <smiths37@gmail.com>
2022-02-19 13:03:14 -0800

    Adjust media query for tablets. Remove unused script. Fine tune CSS. Add site title. Add NPM, javascript libraries.

--- Fruit Smash/wwwroot/js/site.js
+++ Fruit Smash/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿import { BeerQuote } from "./beerq/beerq.js"
 
-// Write your JavaScript code.
+let beerQuote = BeerQuote("eng");
+
+let randomQuote = beerQuote.getRandomQuote();
+
+console.log(randomQuote.quote);
+
